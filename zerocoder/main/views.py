c0cfd06a250ebbd21d234f65a81577a37ac3831a
defstate911 <9114404@gmail.com>
2025-01-28 17:31:47 +0300

    Delete zerocoder directory

--- zerocoder/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('<h1>Это мой первый проект на Django!!!!</h1>')
-
-def new(request):
-    return HttpResponse('<h1>Это вторая страница моего  проекта на Django!!!!</h1>')
